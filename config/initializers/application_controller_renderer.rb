ac6cc5aeea50a58fc27a9b7ac4fd6bffc39c1017
katakurashota <j230347g9327@gmail.com>
2021-09-26 17:00:44 +0000

    [4-3]finish

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '12cbff46e2db6b55632841a39fcdf165c0c8521473c94964a5b2f1f04308c188846cb299cdb8316dfada982e531aec3f007689b9a395b2e8ec17ecaf37d0e791'
